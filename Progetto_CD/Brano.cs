b8799ba31880bace8e460008461f63c168b10ea5
Enes Sela <ENES.SELA@local.ittcesena.it>
2022-11-10 09:24:27 +0100

    versione classe brano 1.0

--- Progetto_CD/Brano.cs
+++ Progetto_CD/Brano.cs
@@ -8,5 +8,32 @@ namespace Progetto_CD
 {
     internal class Brano
     {
+        public string titolo { get; set; }
+        public string autore { get; set; } 
+        public int durata { get; set; }
+
+        internal Brano(string Titolo,string Autore,int Durata) 
+        {
+            this.titolo = Titolo;
+            this.autore = Autore;
+            this.durata = Durata;
+        }
+
+        internal string toString() 
+        {
+            return "Autore : " + autore + "Titolo : " + titolo + "Durata : " + durata;
+        }
+
+        internal bool shortSong(int d) 
+        {
+            if (durata < d)
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
     }
 }
